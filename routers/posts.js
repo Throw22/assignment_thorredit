d05886eeec214b5ccd4be5d2aeeea00915b67b49
nicoasp <nicolas.amaya@gmail.com>
2017-04-18 00:22:12 +0200

    add new comment functionality

--- routers/posts.js
+++ routers/posts.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var models = require('./../models');
 var Post = mongoose.model('Post');
+var User = mongoose.model('User');
+var Comment = mongoose.model('Comment');
+
 
 // ----------------------------------------
 // Index
@@ -40,5 +43,61 @@ router.get('/:id', (req, res) => {
     })
     .catch(e => res.status(500).send(e.stack));
 });
+
+// ----------------------------------------
+// New comment
+// ----------------------------------------
+router.get('/:postid/newcomment/:parentid', (req, res) => {
+  let postid = req.params.postid;
+  let parentid = req.params.parentid;
+  if (postid === parentid) {
+    Post.findById(postid)
+      .then(commentable => {
+        let parentType = "post";
+        res.render('comment/new', { commentable, postid, parentid, parentType })
+      })    
+  } else {
+    Comment.findById(parentid)
+      .then(commentable => {
+        let parentType = "comment";
+        res.render('comment/new', { commentable, postid, parentid, parentType })
+      })  
+  }
+})
+
+// ----------------------------------------
+// Post new comment
+// ----------------------------------------
+router.post('/newcomment', (req, res) => {
+  let postid = req.body.postid;
+  let parentid = req.body.parentid;
+  User.findById('58f535423c62619d2594697b').then((author) => {
+    var comment = new Comment({
+      body: req.body.body,
+      author: author,
+      score: 0
+    });
+    comment.save();
+    if (postid === parentid) {
+      Post.findById(postid).then((post) => {
+        post.comments.push(comment);
+        post.save().then(() => {
+          res.redirect(`${postid}`);
+        })        
+      })
+    } else {
+      Comment.findById(parentid).then((parentComment) => {
+        parentComment.comments.push(comment);
+        parentComment.save().then(() => {
+          res.redirect(`${postid}`);
+        })        
+      })
+    }
+  })
+
+})
+
+
+
 
 module.exports = router;
